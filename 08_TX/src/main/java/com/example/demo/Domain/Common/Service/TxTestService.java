84b2b987d4203a1cb6cbbee8e017ca4808277b05
jiwon <jiwonsukjun@naver.com>
2025-09-05 17:49:10 +0900

    커밋커밋

--- 08_TX/src/main/java/com/example/demo/Domain/Common/Service/TxTestService.java
+++ 08_TX/src/main/java/com/example/demo/Domain/Common/Service/TxTestService.java
@@ -1,4 +1,58 @@
 package com.example.demo.Domain.Common.Service;
 
+
+import com.example.demo.Domain.Common.Entity.Memo;
+import com.example.demo.Domain.Common.Repository.MemoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
 public class TxTestService {
+    @Autowired
+    private MemoRepository memoRepository;
+
+    public void addMemo() throws Exception
+    {
+        log.info("TxTestService's addMemo");
+        Memo memo = Memo.builder()
+                .id(null)
+                .text("tx")
+                .writer("a")
+                .createAt(LocalDateTime.now())
+                .build();
+        memoRepository.save(memo);        //여러 과정을 실행했다고 가정해서 4개
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+    }
+
+
+    @Transactional(rollbackFor = SQLException.class,transactionManager = "jpaTransactionManager")
+    public void addMemoTx() throws Exception
+    {
+        log.info("TxTestService's addMemoTx");
+        Memo memo = Memo.builder()
+                .id(null)
+                .text("tx")
+                .writer("a")
+                .createAt(LocalDateTime.now())
+                .build();
+        memoRepository.save(memo);        //여러 과정을 실행했다고 가정해서 4개
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+    }
+
 }
--- 08_TX/src/test/java/com/example/demo/Domain/Common/Dao/MemoDaoTest.java
+++ 08_TX/src/test/java/com/example/demo/Domain/Common/Dao/MemoDaoTest.java
@@ -1,13 +1,7 @@
 package com.example.demo.Domain.Common.Dao;
 
-import com.example.demo.Domain.Common.Dto.MemoDto;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
@@ -16,11 +10,11 @@ class MemoDaoTest {
     @Autowired
     private MemoDao memoDao;
 
-    @Test
-    public void t1()throws SQLException{
-        memoDao.insert(new MemoDto(20L,"내용","user111@naver.com",LocalDateTime.now()));
-
-    }
+//    @Test
+//    public void t1()throws SQLException{
+//        memoDao.insert(new MemoDto(20L,"내용","user111@naver.com",LocalDateTime.now()));
+//
+//    }
 
 
 }
